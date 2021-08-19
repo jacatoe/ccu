4d6df99ffba50fd85a37643c811c1645e8239869
jacatoe <88354953+jacatoe@users.noreply.github.com>
2021-08-19 18:49:05 -0400

    Deleted all files.

--- ccu/introduction-to-parallel-systems/mpi02/functions.h
+++
@@ -1,8 +0,0 @@
-/* 
-	Joshua Catoe
-	CSCI 473
-	MPI02
-	functions.h - Prototypes the global_sum() function.
-*/
-
-void global_sum(double*, int, int, double);
--- ccu/introduction-to-parallel-systems/mpi04/utilities.h
+++
@@ -1,3 +0,0 @@
-void malloc2D(double*** a, int jmax, int imax);
-void fill2D(double** , int , int );
-void print2D(double** a, int jmax, int imax);
