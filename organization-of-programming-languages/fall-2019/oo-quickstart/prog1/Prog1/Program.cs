c4ff10690b75876d8cca9e42ba47c3b53aee4141
jacatoe <88354953+jacatoe@users.noreply.github.com>
2021-08-21 19:59:03 -0400

    Added fall-2019 section.

---
+++ organization-of-programming-languages/fall-2019/oo-quickstart/prog1/Prog1/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Joshua Catoe
+ * CSCI 350
+ * Prog1
+*/
+
+using System;
+using System.IO;
+
+namespace Prog1
+{
+    class RedboxV1_0
+    {
+        static void Main(string[] args)
+        {
+            // Read input from file 
+
+            FileStream file = new FileStream("movies.txt", FileMode.Open, FileAccess.Read, FileShare.Read);  //  Open file for reading
+
+            StreamReader stream = new StreamReader("movies.txt");  // Create StreamReader to read from file
+
+            string entry;  //  Variable to store each line of file
+
+            string name;    //  Name of movie
+            string year;    //  Year of movie's release
+            string author;  //  Author of movie
+            string rating;  //  Rating of movie
+
+            while ((entry=stream.ReadLine()) != null)  //  Whenever a line is read, do the following
+            {
+                name = entry.Substring(0, entry.IndexOf("("));  //  Parse name from line
+
+                year = entry.Substring(entry.IndexOf("(") + 1, (entry.IndexOf(")")) - (entry.IndexOf("(") + 1));  //  Parse year from line
+
+                author = entry.Substring(entry.IndexOf("\"") + 1, (entry.LastIndexOf("\"")) - (entry.IndexOf("\"") + 1));  //  Parse author from line
+
+                rating = entry.Substring(entry.IndexOf(".") - 1, (entry.IndexOf(".") + 1) - (entry.IndexOf(".") - 2));  //  Parse rating from line
+
+                Console.WriteLine($"Name: {name}\nYear: {year}\nAuthor: {author}\nRating: {rating}\n");  //  Print movie information to console
+            }
+
+            file.Close();  //  Close file
+
+            Console.WriteLine("Press any key to close.");  //  Prompt user to close program
+
+            Console.Read(); //  Read input to close program
+
+            
+        }
+    }
+}
